1d83dcf54d40f20748748c62882bd24469418850
drkrenn <140557188+drkrenn@users.noreply.github.com>
2023-12-07 17:24:24 -0600

    upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,6 +12,7 @@
     const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
     
     d3.json(url).then(function(data) {
+
       // Corrected function name from L.geojson to L.geoJson
       L.geoJson(data, {
           // Corrected the pointToLayer function
@@ -20,34 +21,42 @@
           },
           
           // set style for markers
-          style: setStyle
-          }).addTo(myMap);
-    
-          // Size
-          function setStyle(feature) {
-            return {
-              radius: markerSize(feature.properties.mag),
-            };
-          }
-    
-          function markerSize(magnitude) {
-            return magnitude * 2;
-          };
+          style: setStyle,
 
-          //color
-          function setStyle(feature) {
-            return {
-              radius: markerSize(feature.properties.mag),
-            };
-          }
     
-          function markerSize(magnitude) {
-            return magnitude * 2;
-          };
+
+//Pop up
           onEachFeature: function(feature, layer) {
-            layer.bindPopup("<Magnitude>" + feature.properties.mag + "Depth" + feature.geometry.coordinates[2]);
-          };     
-          style: setStyle
-
+            layer.bindPopup("Magnitude- " + feature.properties.mag + "<br>Depth- " + feature.geometry.coordinates[2])
+          }    
+        }).addTo(myMap);
       console.log(data)
     });
+           // Size and Color   
+    function getColor(d) {
+      return d > 15 ? '#3D3D3D' :
+             d > 11  ? '#8c2d04' :
+             d > 8  ? '#cc4c02' :
+             d > 4   ? '#ec7014' :
+             d > 3   ? '#fe9929' :
+             d > 2   ? '#fff7bc' :
+                        '#ffffe5';
+  }
+
+    function setStyle(feature) {
+      return {
+        radius: markerSize(feature.properties.mag),
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        weight: 1,
+        opacity: 100,
+        color: getColor(feature.geometry.coordinates[2]),
+        fillOpacity: 0.7
+      };
+    }
+
+    function markerSize(magnitude) {
+      return magnitude * 3;
+    };
+
+
+L.geoJson(Data, {style: style}).addTo(myMap);
